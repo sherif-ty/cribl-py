e055211e0faecc0b4ce580964fc0187d4bc03860
sherif-abdelkader1_voda <sherif.abdelkader1@vodafone.com>
2025-05-06 10:04:55 +0300

    edit win

--- edge-installation/windows_installation.py
+++ edge-installation/windows_installation.py
@@ -7,9 +7,11 @@ def install_windows(config):
     # Construct the msiexec command using values from the config file
     command = (
         f'msiexec /i "{config["For_Windows_cribl_pkg_url"]}" /qn '
-        f'MODE=mode-managed-edge HOSTNAME={config["LEADER_IP"]} PORT=4200 '
-        f'AUTH={config["EDGE_TOKEN"]} FLEET={config["FLEET_NAME"]} '
-        f'TLS_DISABLED={config["TLS_DISABLED"]}'
+        f'MODE="mode-managed-edge" HOSTNAME="{config["LEADER_IP"]}" PORT="4200" '
+        f'AUTH="{config["EDGE_TOKEN"]}" FLEET="{config["FLEET_NAME"]}" '
+        f'TLS="{config["TLS"]}" USERNAME="LocalSystem" '
+        f'APPLICATIONROOTDIRECTORY="C:\\Program Files\\Cribl\\" '
+        f'/l*v "{config["LOG_PATH"]}"'
     )
     
     if platform.system() == "Windows":
@@ -21,14 +23,11 @@ def install_windows(config):
             # Restart the Cribl service
             subprocess.run("net stop cribl", shell=True)
             subprocess.run("net start cribl", shell=True)
-            # print(f"Return code: {result.returncode}")
-            # print(f"Standard output: {result.stdout}")
             print("Installation done successfully.")
-
         else:
             raise RuntimeError(f"Installation failed with return code {result.returncode}")
     else:
         print("This script must be run in a Windows environment.")
 
-        
-        
+
+
